79d171d896344420e43c920cba6c00991226e467
vivek-khandare <khandarevivek78@gmail.com>
2026-05-16 19:25:25 +0530

    Create data.tf

---
+++ data.tf
@@ -0,0 +1,13 @@
+# ✅ Fixed - Amazon Linux 2023 (Java 21 supported)
+data "aws_ami" "latest-amazon-linux-image" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
