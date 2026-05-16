dcfff36dc73bcba07c7d14d396656f5d18980059
vivek-khandare <khandarevivek78@gmail.com>
2026-05-16 19:24:23 +0530

    Update jenkins-server-script.sh

--- jenkins-server-script.sh
+++ jenkins-server-script.sh
@@ -1,15 +1,49 @@
 #!/bin/bash
 
 # install jenkins
-sudo yum update –y
+sudo yum update -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo yum upgrade
-sudo yum install java-17-amazon-corretto -y
+sudo yum upgrade -y
+sudo yum install java-21-amazon-corretto -y
 sudo yum install jenkins -y
+sudo systemctl daemon-reload
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
+
+
+# #!/bin/bash
+
+# # install jenkins
+# sudo yum update -y
+# sudo yum upgrade -y
+
+# # Install Java 21
+# sudo yum install java-21-amazon-corretto -y
+
+# # Add Jenkins repo manually (wget unreliable in user_data)
+# sudo tee /etc/yum.repos.d/jenkins.repo > /dev/null <<'EOF'
+# [jenkins]
+# name=Jenkins
+# baseurl=https://pkg.jenkins.io/redhat-stable
+# gpgcheck=1
+# gpgcakey=https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+# EOF
+
+# # Import GPG key
+# sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+
+# # Install Jenkins (nogpgcheck to avoid GPG issues)
+# sudo yum install jenkins --nogpgcheck -y
+
+# # Start Jenkins
+# sudo systemctl daemon-reload
+# sudo systemctl enable jenkins
+# sudo systemctl start jenkins
+
+
+
 
 
 # install git
