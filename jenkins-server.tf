765d00f508f0f857a6487742b9305b3f4a92c71d
vivek-khandare <khandarevivek78@gmail.com>
2026-05-16 19:26:03 +0530

    Update jenkins-server.tf

--- jenkins-server.tf
+++ jenkins-server.tf
@@ -1,16 +1,3 @@
-data "aws_ami" "latest-amazon-linux-image" {
-  most_recent = true
-  owners      = ["amazon"]
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
 resource "aws_instance" "myapp-server" {
   ami                         = data.aws_ami.latest-amazon-linux-image.id
   instance_type               = var.instance_type
